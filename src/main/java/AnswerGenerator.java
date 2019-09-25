647dab984dc40cab26d8c066ef9945194a31666f
sagittatius <qiaoliang.email@gmail.com>
2019-09-25 20:23:38 +0800

    Create an answer with  4 numbers, and any of those numbers are different with each other.

--- src/main/java/AnswerGenerator.java
+++ src/main/java/AnswerGenerator.java
@@ -1,7 +1,20 @@
+import java.util.*;
+
 public class AnswerGenerator {
 
-
     public String createAnAnswer() {
-        return "1 2 3 4";
+        ArrayList <String> t = new ArrayList <>() ;
+        t.addAll(Arrays.asList(new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
+        StringBuffer result = new StringBuffer();
+        while(!isEnoughNumbers(t)) {
+            int position = new Random().nextInt(t.size());
+            result.append(" ").append(t.get(position));
+            t.remove(position);
+        }
+        return result.toString().trim();
+    }
+
+    private boolean isEnoughNumbers(List<String> t) {
+        return t.size()<7;
     }
 }
--- src/test/java/AwnserGeneratorTest.java
+++ src/test/java/AwnserGeneratorTest.java
@@ -11,13 +11,18 @@ public class AwnserGeneratorTest {
     public void
     should_generate_an_answer_of_four_numbers_and_those_numbers_is_different_with_each_other()  {
         String result = new AnswerGenerator().createAnAnswer();
-        String[] answers = result.split(" ");
-        Set box = new HashSet<Integer>();
+        String[] answers = result.split("\\s");
+        assertThat(answers.length,is(4));
+        assertEachNumberIsdefferent(answers);
+    }
+
+    private void assertEachNumberIsdefferent(String[] answers) {
+        Set box = new HashSet <Integer>();
         for (String each :
                 answers) {
-            Integer actual = Integer.valueOf(each).intValue();
+            Integer actual = Integer.valueOf(each);
             assertThat(actual, isA(Integer.TYPE));
-            box.add(actual);
+            box.add(actual.intValue());
         }
         assertThat(box.size(),is(4));
     }
