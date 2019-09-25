bf5b19b170713031ee72d5fa9911293ba06298b0
sagittatius <qiaoliang.email@gmail.com>
2019-09-25 20:35:46 +0800

    refectoring: extract the mathod drawNumberFromListRondomly().

--- src/main/java/AnswerGenerator.java
+++ src/main/java/AnswerGenerator.java
@@ -2,19 +2,25 @@ import java.util.*;
 
 public class AnswerGenerator {
 
+    public static final String SPLITTER = " ";
+
     public String createAnAnswer() {
         ArrayList <String> t = new ArrayList <>() ;
         t.addAll(Arrays.asList(new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}));
-        StringBuffer result = new StringBuffer();
+        String answer = "";
         while(!isEnoughNumbers(t)) {
-            int position = new Random().nextInt(t.size());
-            result.append(" ").append(t.get(position));
-            t.remove(position);
+            answer += drawNumberFromListRondomly(t);
         }
-        return result.toString().trim();
+        return answer.trim();
     }
 
     private boolean isEnoughNumbers(List<String> t) {
         return t.size()<7;
+    }
+    private String drawNumberFromListRondomly(List<String> t){
+        int position = new Random().nextInt(t.size());
+        String result=t.get(position);
+        t.remove(position);
+        return SPLITTER +result;
     }
 }
