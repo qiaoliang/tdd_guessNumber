306ee67d22a2327c68f505ff267e1d080d05aa94
sagittatius <qiaoliang.email@gmail.com>
2019-10-07 21:27:23 +0800

    玩家最多猜测六次。

--- src/main/java/BoxGameController.java
+++ src/main/java/BoxGameController.java
@@ -6,24 +6,25 @@ public class BoxGameController {
     private AnswerInput answerInput = null;
     private RandomAnswerGenerator randomAnswerGenerator =null;
     private String answer ="";
-
-
-    public BoxGameController(RandomAnswerGenerator randomAnswerGenerator) {
-        this(randomAnswerGenerator,null);
-    }
+    private int round = 0;
 
     public BoxGameController(RandomAnswerGenerator randomAnswerGenerator, AnswerInput answerInput) {
         this.randomAnswerGenerator= randomAnswerGenerator;
         this.answerInput = answerInput;
-    }
-    public void startGame(){
-        BoxGame game = new BoxGame(randomAnswerGenerator.createAnAnswer());
-        answer = game.guess(answerInput.input());
     }
     public String getGameResult() {
         if(answer.equals("4A0B"))
             return "player won!";
         else
             return "player lose!";
+    }
+
+    public void startGameWithinMaxRounds(int maxRounds) {
+        BoxGame game = new BoxGame(randomAnswerGenerator.createAnAnswer());
+        for (round = 0; round < maxRounds; round++) {
+            answer = game.guess(answerInput.input());
+            if (answer.equals("4A0B"))
+                return;
+        }
     }
 }
--- src/test/java/BoxGameControllerTest.java
+++ src/test/java/BoxGameControllerTest.java
@@ -1,38 +1,38 @@
 import org.junit.jupiter.api.Test;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class BoxGameControllerTest {
+
+        @Test public void
+        should_exit_after_guess_6_times_with_wrong_answers() throws Exception{
+            RandomAnswerGenerator randomAnswerGenerator = mock(RandomAnswerGenerator.class);
+            AnswerInput answerInput = mock(AnswerInput.class);
+            when(randomAnswerGenerator.createAnAnswer()).thenReturn("1 2 3 4");
+            String firsWrongtAnswer = "5 6 7 8";
+            String[] nextFiveWrongAnswersGuessed= new String[]{"5 7 8 9","8 7 6 9","4 3 2 1","5 1 3 4","7 1 3 4"};
+            when(answerInput.input()).thenReturn(firsWrongtAnswer,nextFiveWrongAnswersGuessed);
+
+        BoxGameController boxGameController = new BoxGameController(randomAnswerGenerator,answerInput);
+        boxGameController.startGameWithinMaxRounds(6);
+        assertThat(boxGameController.getGameResult(),is("player lose!"));
+    }
+
     @Test public void
-    should_exit_with_success_when_guess_correctly() throws Exception{
+    should_exit_immediately_as_soon_as_the_answer_is_correct_and_total_times_is_no_more_than_6_times() throws Exception{
         RandomAnswerGenerator randomAnswerGenerator = mock(RandomAnswerGenerator.class);
         AnswerInput answerInput = mock(AnswerInput.class);
         when(randomAnswerGenerator.createAnAnswer()).thenReturn("1 2 3 4");
-        when(answerInput.input()).thenReturn("1 2 3 4");
+        String firsWrongtAnswer = "5 6 7 8";
+        String[] nextFiveAnswersGuessedWithLastRight= new String[]{"5 7 8 9","8 7 6 9","4 3 2 1","5 1 3 4","1 2 3 4"};
+        when(answerInput.input()).thenReturn(firsWrongtAnswer,nextFiveAnswersGuessedWithLastRight);
 
         BoxGameController boxGameController = new BoxGameController(randomAnswerGenerator,answerInput);
-        boxGameController.startGame();
-
+        boxGameController.startGameWithinMaxRounds(6);
         assertThat(boxGameController.getGameResult(),is("player won!"));
     }
 
-    @Test public void
-    should_exit_with_failed_when_guess_answer_is_wrong() throws Exception{
-        RandomAnswerGenerator randomAnswerGenerator = mock(RandomAnswerGenerator.class);
-        AnswerInput answerInput = mock(AnswerInput.class);
-        when(randomAnswerGenerator.createAnAnswer()).thenReturn("1 2 3 4");
-        when(answerInput.input()).thenReturn("5 6 7 8");
-
-        BoxGameController boxGameController = new BoxGameController(randomAnswerGenerator,answerInput);
-        boxGameController.startGame();
-
-        assertThat(boxGameController.getGameResult(),is("player lose!"));
-    }
 }
